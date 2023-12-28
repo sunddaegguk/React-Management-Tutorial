4ad08e46cd2d9155c75dac8dbc4cd1c7d03cb02f
sunddaegguk <ssitek22@gmail.com>
2023-12-28 20:57:33 +0900

    Create Customer.js

--- react.project/my-app/src/App.js
+++ react.project/my-app/src/App.js
@@ -1,13 +1,54 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Lets' develop management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+    id: 1,
+    image: 'https://source.unsplash.com/user/c_v_r/100x100',
+    name: '김일순',
+    birthday: '961225',
+    gender: 'male',
+    job: 'university student',
+  },
+  {
+    id: 2,
+    image: 'https://source.unsplash.com/user/c_v_r/100x100',
+    name: '김이순',
+    birthday: '661225',
+    gender: 'female',
+    job: 'highschool student',
+  },
+  {
+    id: 1,
+    image: 'https://source.unsplash.com/user/c_v_r/100x100',
+    name: '김산순',
+    birthday: '990205',
+    gender: 'male',
+    job: 'middle student',
+  },
+];
+class App extends Component {
+  render() {
+    return (
+      <>
+        {customers.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </>
+    );
+  }
 }
+//map 배열 함수를 쓸 경우 key값을 넣어줘야한다.
 
 export default App;
